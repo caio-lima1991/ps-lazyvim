d10f8e6fc37e8b05dbfb8e91f7d5e18ed14395b2
Caio Lima <caio.lima1991@icloud.com>
2025-04-15 06:55:36 -0300

    correcao: removendo telescope e corrigindo formatter

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -4,7 +4,6 @@ return {
     jdtls = function(opts)
       local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
       local jvmArg = "-javaagent:" .. install_path .. "/lombok.jar"
-      table.insert(opts.cmd, "--jvm-arg=" .. jvmArg)
       table.insert(opts.cmd, "--java-executable=/usr/lib/jvm/java-21-openjdk-amd64/bin/java")
 
       opts.settings = {
@@ -20,19 +19,22 @@ return {
                 path = "/opt/jvm/amazon-corretto-11.0.26.4.1-linux-x64",
               },
               {
-                name = "JavaSE-17",
-                path = "/usr/lib/jvm/java-17-openjdk-amd64",
-              },
-              {
                 name = "JavaSE-21",
                 path = "/usr/lib/jvm/java-21-openjdk-amd64",
+              },
+              {
+                name = "JavaSE-17",
+                path = "/usr/lib/jvm/java-17-openjdk-amd64",
               },
             },
           },
           format = {
             enabled = true,
+            comments = {
+              enabled = true,
+            },
             settings = {
-              url = vim.fn.expand("~/.config/nvim/lua/resources/intellij-java-google-style.xml"),
+              url = "~/.config/nvim/lua/resources/eclipse-java-google-style.xml",
               profile = "GoogleStyle",
             },
           },

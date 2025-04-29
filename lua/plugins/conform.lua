ed1f7387e5efe70b751463d1be50f34d05a98ea6
Caio Lima <caio.lima1991@icloud.com>
2025-04-29 10:24:02 -0300

    feature: adicionando conforme ao projeto, astyle

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,18 +1,20 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "black", "isort" },
-        javascript = { "prettierd", "eslint_d" },
-        typescript = { "prettierd", "eslint_d" },
-        java = { "astyle" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
-      -- Você pode adicionar outras opções de configuração do conform aqui, se desejar.
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black", "isort" },
+			javascript = { "prettierd", "eslint_d" },
+			typescript = { "prettierd", "eslint_d" },
+			java = { "astyle" },
+			sql = { "sqlfluff" },
+		},
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		},
+		-- Você pode adicionar outras opções de configuração do conform aqui, se desejar.
+	},
 }
+
